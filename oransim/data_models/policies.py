eb54ec8c11642bbd039510e1ae6139f9656fb712
Jupiter-Plantagenet <georgejupiter303@gmail.com>
2025-01-30 22:18:14 +0900

    restructured file and folder structure

--- oransim/data_models/policies.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel  
-
-class A1Policy(BaseModel):  
-    policy_id: int  
-    policy: str          # Simplified for now; later use ENUMs  
-    validity_window: tuple = (0, 0)  # Start/end timestamps  
