d3e145eb40ed02269fdae33a7c78b554c415e4db
George Akor <georgejupiter303@gmail.com>
2025-02-01 16:16:07 +0900

    feat: Initialize xapp_rapp_framework subpackage
    This commit initializes the `xapp_rapp_framework` subpackage, which provides
    a framework for developing and deploying xApps and rApps in ORANSim.
    
    The `__init__.py` file for the `xapp_rapp_framework` subpackage is added to
    mark it as a Python package.

---
+++ oransim/xapp_rapp_framework/__init__.py
@@ -0,0 +1,10 @@
+"""
+The `xapp_rapp_framework` subpackage provides a framework for developing and 
+deploying xApps and rApps within the ORANSim simulation environment.
+
+It includes base classes for xApps and rApps, as well as utilities for 
+interacting with the Near-RT RIC and Non-RT RIC, respectively.
+"""
+
+# You can add any package-level imports or variables here if needed.
+# For now, we will keep this __init__.py file minimal.
