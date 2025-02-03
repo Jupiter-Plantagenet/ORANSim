73fa75774591208b8702e99c83d88416dcb25dd0
George Akor <georgejupiter303@gmail.com>
2025-02-03 15:13:33 +0900

    docs: Add Sphinx configuration file
    This commit introduces the Sphinx configuration file (`conf.py`) for
    generating the ORANSim library documentation.
    
    The configuration file includes:
    
    - Project information (name, copyright, author, version).
    - Enabled extensions (`duration`, `doctest`, `autodoc`, `autosummary`, `napoleon`).
    - Basic HTML output settings (theme set to 'alabaster').
    
    This sets up the foundation for generating high-quality documentation
    from docstrings and other sources.

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ORANSim'
+copyright = '2023, Your Name or Organization'
+author = 'Your Name or Organization'
+release = '0.1.0' # Update with your project's version number
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'  # You can choose a different theme
+html_static_path = ['_static']
